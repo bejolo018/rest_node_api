089d219ce56a8cce2450861e8618d439a206447e
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-10-05 15:05:54 -0500

    created seperate folder for route as well as used middleware in app js to simplify routes in posts

--- app.js
+++ app.js
@@ -3,19 +3,15 @@ const app = express();
 const mongoose = require('mongoose');
 require('dotenv/config')
 
-// ROUTES
-app.get('/', (req, res) => {
-    res.send('We are on home')
-});
+//Import Routes
+const postRoute = require('./Routes/posts');
 
-app.get('/posts', (req, res) => {
-    res.send('We are on posts')
-});
+// Middleware
+app.use('/posts', postRoute);
 
 // Connect to DB
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true },  () => {
     console.log('connected to db!')
 })
 
-// How to start listening to the server
 app.listen(3000)
