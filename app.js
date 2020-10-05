42ab893df09aa74a26dc79c778ea59a82528be0f
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-10-05 15:11:42 -0500

    Created post model

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const postRoute = require('./Routes/posts');
 // Middleware
 app.use('/posts', postRoute);
 
+// Routes
+app.get('/', (req, res) => {
+    res.send('We are on home')
+});
+
 // Connect to DB
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true },  () => {
     console.log('connected to db!')
