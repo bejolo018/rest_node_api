9bf4e21ac98e3904b92d88fac4c6936a6670df2c
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-10-05 14:56:42 -0500

    Used mongoose to connect to database, used dotenv package and file to mask our db user and password

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config')
 
 // ROUTES
+app.get('/', (req, res) => {
+    res.send('We are on home')
+});
+
+app.get('/posts', (req, res) => {
+    res.send('We are on posts')
+});
+
+// Connect to DB
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true },  () => {
+    console.log('connected to db!')
+})
+
+// How to start listening to the server
+app.listen(3000)
