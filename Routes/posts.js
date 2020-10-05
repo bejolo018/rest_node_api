b1cc24d7d7eaa78a30c44f1302e3a2b072699ccb
Benjamin Lopez <47269255+bejolo018@users.noreply.github.com>
2020-10-05 15:25:29 -0500

    created post functionality using async

--- Routes/posts.js
+++ Routes/posts.js
@@ -1,14 +1,24 @@
 const express = require('express');
-
 const router = express.Router();
+const Post = require('../Models/Post');
 
 // ROUTES
 router.get('/', (req, res) => {
     res.send('We are on posts')
 });
 
-router.get('/specific', (req, res) => {
-    res.send('specific post')
-});
+router.post('/', async (req, res) => {
+    const post = new Post({
+        title: req.body.title,
+        description: req.body.description
+    });
+
+    try{
+    const savedPost = await post.save()
+        res.json(savedPost)
+    } catch (err){
+        res.json({message: error})
+    }
+})
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 require('dotenv/config')
+
+app.use(bodyParser.json());
 
 //Import Routes
 const postRoute = require('./Routes/posts');
@@ -19,4 +22,4 @@ mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true },  () => {
     console.log('connected to db!')
 })
 
-app.listen(3000)
+app.listen(5000)
